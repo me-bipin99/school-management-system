18c882d71744b501762e535ce2ac1a921e9e2ec9
Bipin Parajuli <parajuli.bip99@gmail.com>
2026-05-28 20:47:29 +0545

    Refactor navbar structure and update layout

--- navbar.php
+++ navbar.php
@@ -1,35 +1,39 @@
 <!-- navigation -->
- <section class="navigation">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top px-lg-5 px-3">
-      <a class="navbar-brand order-2 order-lg-0 mr-auto mx-lg-3" href="index.php"><b>SMS</b></a>
-      <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<section class="navigation">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top px-lg-5 px-3">
+    <!-- Brand: left -->
+    <a class="navbar-brand" href="index.php"><b>SMS</b></a>
 
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav main-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#about-us">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#courses">Programs</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#updates">Recent Updates</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#contact-us">Contact-us</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav nav-flex-icons ml-auto">
-          <li class="nav-item">
-            <a href="admin/dashboard.php" class="text-light text-decoration-none"><i class="fas fa-user text-light mr-2"></i><strong>Login</strong></a>
-          </li>
-        </ul>
-      </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    </nav>
-  </section>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <!-- Nav links: centered in the middle -->
+      <ul class="navbar-nav main-nav mx-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about-us">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#courses">Programs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#updates">Recent Updates</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact-us">Contact-us</a>
+        </li>
+      </ul>
+
+      <!-- Login: right -->
+      <ul class="navbar-nav nav-flex-icons">
+        <li class="nav-item">
+          <a href="admin/dashboard.php" class="text-light text-decoration-none"><i class="fas fa-user text-light mr-2"></i><strong>Login</strong></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</section>
